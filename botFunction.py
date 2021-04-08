361384d741c18123a58b279336dc289d74bac9d3
Xiao Xiong <brockdw@outlook.com>
2021-04-08 04:24:40 -0400

    oop the program, adding handleOpen function

--- botFunction.py
+++ botFunction.py
@@ -1,6 +1,88 @@
 import cv2
 import pyautogui
 import time
+# import logging
+
+# logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s')
+log_format = "{:>40} -- {:>10}"
+starCount = 100
+
+
+
+class Action:
+    def __init__(self):
+        self.sleepTime = 6
+        pass
+
+    def perform_action(self):
+        pass
+
+    def clickOnImg(self, imgUrl, confidenceValue):
+        try:
+            centerPoint = pyautogui.locateCenterOnScreen(imgUrl, confidence=confidenceValue)
+            if (centerPoint):
+                pyautogui.click(centerPoint[0], centerPoint[1])
+            else:
+                print(log_format.format(imgUrl, "button not found, please adjust your screen"))
+        except OSError:
+            print(log_format.format(imgUrl, "image file did not exists"))
+        time.sleep(2)
+
+        self.waitForImg("./botImg/waitingIcon.png", 5, 0.7)
+        self.waitForImg("./botImg/battle/endGameCutPic.png", 5, 0.8)
+        self.waitForImg("./botImg/baseIntro.png", 5, 0.8)
+        print("*"*starCount)
+
+    def checkExist(self, imgUrl, confidenceValue):
+        try:
+            centerPoint = pyautogui.locateCenterOnScreen(imgUrl, confidence=confidenceValue)
+            if (centerPoint):
+                return True
+            else:
+                print(log_format.format(imgUrl, "did not exist on screen"))
+                return False
+        except OSError:
+            print(log_format.format(imgUrl, "image file did not exists"))
+            return False
+
+    def waitForImg(self, imgUrl, waitTime, curConf):
+        counter = 0
+        while (pyautogui.locateCenterOnScreen(imgUrl, confidence=curConf) != None):
+            print(log_format.format(imgUrl, "iteration: " + str(counter)))
+            counter += 1
+            time.sleep(waitTime)
+        print(log_format.format(imgUrl, "has stopped"))
+        # print()
+
+class HandleOpen(Action):
+    def __init__(self):
+        pass
+
+    def perform_action(self):
+        self.clickOnImg("./botImg/closePanel.png", 0.8)
+        self.clickOnImg("./botImg/supplyIcon.png", 0.8)
+        self.clickOnImg("./botImg/closePanelSecond.png", 0.8)
+        self.clickOnImg("./botImg/homePage/publicRecrute.png", 0.8)
+        while self.checkExist("./botImg/homePage/recrute_confirm.png", 0.8):
+            self.clickOnImg("./botImg/homePage/recrute_confirm.png", 0.8)
+            self.clickOnImg("./botImg/draw_confirm.png",0.8)
+            pyautogui.click(50,50)
+
+        while self.checkExist("./botImg/homePage/begin_new_recrute.png", 0.8):
+            self.clickOnImg("./botImg/homePage/begin_new_recrute.png", 0.8)
+            self.clickOnImg("./botImg/draw_confirm.png",0.8)
+            pyautogui.click(50,50)
+        # self.clickOnImg("./botImg/draw_confirm.png", 0.8)
+        # pyautogui.click(50, 50)
+        pass
+
+
+if __name__ == '__main__':
+    handle_open = HandleOpen()
+    handle_open.perform_action()
+
+
+
 
 # baseButtonLocation = pyautogui.locateOnScreen("C:/Users/Brock/OneDrive/Documents/python/arknightBot/venv/Include/botImg/homePage/base.png", confidence = 0.5)
 sleepTime = 6
@@ -20,6 +102,7 @@ def clickOnImg(imgUrl, confidenceValue):
         if(centerPoint):
             pyautogui.click(centerPoint[0], centerPoint[1])
         else:
+            print(log_format.format(imgUrl, "button not found, please adjust your screen"))
             print(imgUrl + "\t\t\tbutton not found, please adjust your screen")
     except OSError:
         print(imgUrl + "\t\t\tdid not exists")
@@ -57,6 +140,7 @@ def click(x, y):
     time.sleep(1)
 
 def battleControl(times):
+    # logging.info("I am here")
     while (times > 0):
         clickOnImg("./botImg/battle/autoPlay.png", 0.8)
         clickOnImg("./botImg/battle/opStart.png", 0.8)
@@ -160,7 +244,7 @@ def chipFarm(chipType, bigChip, times=10):
 # pyautogui.mouseUp()
 #
 # pyautogui.sleep(5)
-battleControl(10)
+
 
 # pyautogui.sleep(5)
 # clickOnImg("./botImg/goBack.png", 0.8)
