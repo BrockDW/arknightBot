5843b37a9f1d291fc0fe3541e6fd2f5193359b14
Brock <brockdw@outlook.com>
2022-01-31 11:31:14 -0500

    update

--- action/action.py
+++ action/action.py
@@ -130,25 +130,25 @@ class Action:
             trial -= 1
             time.sleep(wait_time)
 
-        newDayStarted = False
+        # newDayStarted = False
 
-        if (self.checkExist("./botImg/homePage/daily_message.png", 0.8) or self.checkExist("./botImg/homePage/daily_message_off_select.png", 0.8)):
-            print(log_format.format("a new day has started", "performing basic operation"))
-            self.clickOnImg("./botImg/closePanel.png", 0.8)
-            newDayStarted = True
-            if self.checkExist("./botImg/homePage/daily_reword.png", 0.8):
-                print(log_format.format("no new message today", "reword collected"))
-                self.clickOnImg("./botImg/closePanel.png", 0.8)
-                newDayStarted = True
-
-            if self.checkExist("./botImg/homePage/daily_reword_auto_trigger.png", 0.8):
-                print(log_format.format("no new message today", "reword collected"))
-                self.click_screen()
-                self.clickOnImg("./botImg/closePanel.png", 0.8)
-                newDayStarted = True
-            if newDayStarted:
-                time.sleep(10)
-            return newDayStarted
+        # if (self.checkExist("./botImg/homePage/daily_message.png", 0.8) or self.checkExist("./botImg/homePage/daily_message_off_select.png", 0.8)):
+        #     print(log_format.format("a new day has started", "performing basic operation"))
+        #     self.clickOnImg("./botImg/closePanel.png", 0.8)
+        #     newDayStarted = True
+        #     if self.checkExist("./botImg/homePage/daily_reword.png", 0.8):
+        #         print(log_format.format("no new message today", "reword collected"))
+        #         self.clickOnImg("./botImg/closePanel.png", 0.8)
+        #         newDayStarted = True
+        #
+        #     if self.checkExist("./botImg/homePage/daily_reword_auto_trigger.png", 0.8):
+        #         print(log_format.format("no new message today", "reword collected"))
+        #         self.click_screen()
+        #         self.clickOnImg("./botImg/closePanel.png", 0.8)
+        #         newDayStarted = True
+        #     if newDayStarted:
+        #         time.sleep(10)
+        #     return newDayStarted
 
 
 
